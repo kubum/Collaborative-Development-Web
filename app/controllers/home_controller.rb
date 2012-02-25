c545dd00df6e510c8593374ac4546d169a460f3a
Vladimir Shafikov <vladimir.kersh@gmail.com>
2012-02-25 19:59:50 +0000

    new updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     
   end
+
+  def about
+  	
+  end
+
+  def single
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 CollaborativeDevelopmentWeb::Application.routes.draw do
   root :to => 'home#index'
+  get 'home/about', :to => 'home#about'
+  get 'home/single', :to => 'home#single'
 end
