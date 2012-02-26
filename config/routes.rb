b7d13e1c0af44f3b1d6a62f6a47660edf0880010
Andrey Fadeyev <mass1veffect@gmail.com>
2012-02-26 22:51:02 +0000

    Layout fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CollaborativeDevelopmentWeb::Application.routes.draw do
   root :to => 'home#index'
-  get 'home/about', :to => 'home#about'
+  get 'home/about', :to => 'home#about', :as => :about
   get 'home/single', :to => 'home#single'
   get 'home/shop', :to => 'home#shop'
   get 'home/products', :to => 'home#products'
