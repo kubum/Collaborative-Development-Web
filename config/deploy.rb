06bd7b8b117cff6326da9b704b747c5dcbbc998b
Andrey Fadeyev <mass1veffect@gmail.com>
2012-03-18 20:22:26 +0000

    Production fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,12 +22,11 @@ role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations
 
 # Если хотите поместить конфиг в shared и не хранить его в системе контроя версий - раскомментируйте следующие строки
 
-#after "deploy:update_code", :copy_database_config
+after "deploy:update_code", :copy_database_config
 
 task :copy_database_config, roles => :app do
- run <<-CMD
-   ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml
- CMD
+ db_config = "#{shared_path}/database.yml"
+ run "cp #{db_config} #{release_path}/config/database.yml"
 end
 
 set :unicorn_conf, "/etc/unicorn/university.insight.rb"
@@ -35,7 +34,7 @@ set :unicorn_pid, "/var/run/unicorn/university.insight.pid"
 
 
 
-  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use 1.9.2 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use 1.9.2 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 
 
